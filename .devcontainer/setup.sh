b749c2929545f21ee2ea297f48c9a032693d26f7
Rodrigo de Souza <rsouza01@gmail.com>
2021-03-04 13:38:47 +0000

    Setup fixed + permissions

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -36,5 +36,16 @@ python3.7 -m pip install pip --upgrade pip awscli
 echo "Installing PYTHON - DONE!"
 
 
+echo "Installing oh-my-zsh"
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+cp -R /root/.oh-my-zsh /home/$USERNAME
+cp /root/.zshrc /home/$USERNAME
+sed -i -e "s/\/root\/.oh-my-zsh/\/home\/$USERNAME\/.oh-my-zsh/g" /home/$USERNAME/.zshrc
+chown -R $USER_UID:$USER_GID /home/$USERNAME/.oh-my-zsh /home/$USERNAME/.zshrc
+
+echo "Installing oh-my-zsh - DONE"
+
+
 
 echo ">>>> >>>> >>>> >>>> EXITING SETUP.SH <<<< <<<< <<<< <<<< "
